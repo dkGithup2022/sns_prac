b0829ae5f7894e4a5db89d65250c7239736e83db
dkGithup2022 <104286091+dkGithup2022@users.noreply.github.com>
2023-07-25 20:42:35 +0900

    create post 에 인자 없던것 수정

--- src/main/java/com/dk0124/sns/controller/dto/response/CreatePostResponseDto.java
+++ src/main/java/com/dk0124/sns/controller/dto/response/CreatePostResponseDto.java
@@ -2,5 +2,5 @@ package com.dk0124.sns.controller.dto.response;
 
 import com.dk0124.sns.model.post.PostType;
 
-public record CreatePostResponseDto(){
+public record CreatePostResponseDto(Long postId ){
 }
