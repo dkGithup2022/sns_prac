b54c81ba88dfa257f91a22a554b37df2837eb0fd
dkGithup2022 <104286091+dkGithup2022@users.noreply.github.com>
2023-07-29 14:58:10 +0900

    에러 핸들러에서 unhandled exception 다루도록 변경

--- src/main/java/com/dk0124/sns/common/exception/ExceptionHandler.java
+++ src/main/java/com/dk0124/sns/common/exception/ExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.dk0124.sns.common.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +14,12 @@ public class ExceptionHandler {
 		log.error("{}", e);
 		var errorBody = buildErrorBody(e);
 		return ResponseEntity.status(e.getHttpStatus()).body(errorBody);
+	}
+
+	@org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+	public ErrorResponse catchUnhandledException(Exception e) {
+		log.error("{}", e);
+		return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.toString(), e.getMessage(), "");
 	}
 
 	private ErrorResponse buildErrorBody(ApplicationException e) {
